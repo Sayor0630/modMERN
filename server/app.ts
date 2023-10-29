3f4149b5a1b07a38a956ff9b6d29d154c4421b6e
Sayor0603 <tahsinfaiyazsayor0603@gmail.com>
2023-10-29 20:54:58 +0600

    TFS commit

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,8 @@ import express, { NextFunction, Request, Response } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -17,6 +19,9 @@ app.use(
   })
 );
 
+//routes
+app.use("/api/v1",userRouter);
+
 //testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
@@ -30,3 +35,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+app.use(ErrorMiddleware);
