85e17eac6125c4dcd528914d4ed279351bada9c8
Sayor0603 <tahsinfaiyazsayor0603@gmail.com>
2023-10-30 20:42:14 +0600

    TFS commit

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 const courseRouter = express.Router();
 
 courseRouter.post(
@@ -15,6 +15,16 @@ courseRouter.put(
     isAuthenticated,
     authorizeRoles("admin"),
     editCourse
+);
+
+courseRouter.get(
+    "/get-course/:id",
+    getSingleCourse
+);
+
+courseRouter.get(
+    "/get-courses",
+    getAllCourses
 );
 
 export default courseRouter;
