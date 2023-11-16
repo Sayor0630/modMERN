ebad27807e5dc930fb07802ffbf34ca3182c5343
Sayor0603 <tahsinfaiyazsayor0603@gmail.com>
2023-11-16 21:27:46 +0600

    TFS commit

---
+++ client/app/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React, {FC,useState} from "react"
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+
+
+interface Props {}
+
+
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem,setActiveItem] = useState(0);
+  return (
+    <div>
+      <Heading
+      title="Medical Our Dream"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, quisquam."
+      keywords="Medical, Our Dream, Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, quisquam."
+      />
+      <Header 
+      open={open}
+      setOpen={setOpen}
+      activeItem={activeItem}
+      />
+    </div>
+  )
+};
+
+
+export default Page;
